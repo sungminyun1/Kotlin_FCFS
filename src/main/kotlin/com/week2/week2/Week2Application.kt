364c588dc392a5a5fc3f91ecfdac7d0adb8a4e08
sungminyun1 <smy07207@gmail.com>
2024-09-30 14:11:12 +0900

    init

---
+++ src/main/kotlin/com/week2/week2/Week2Application.kt
@@ -0,0 +1,11 @@
+package com.week2.week2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Week2Application
+
+fun main(args: Array<String>) {
+	runApplication<Week2Application>(*args)
+}
